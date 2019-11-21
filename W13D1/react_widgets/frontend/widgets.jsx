d8d575e8df684a1f63ba901cabe7c794bc43452f
Dave <dave.gagnat@gmail.com>
2019-11-21 13:12:09 -0600

    Rendered root component onto the page

--- W13D1/react_widgets/frontend/widgets.jsx
+++ W13D1/react_widgets/frontend/widgets.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('content');
-  ReactDOM.render(<h1>Widgets APP</h1>, root);
+  ReactDOM.render(<Root />, root);
 });
