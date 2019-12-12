263a294c4129758710d225b23cca393c0a2cf412
Dave <dave.gagnat@gmail.com>
2019-12-11 19:05:27 -0600

    Created folder structure, webpack, dependencies, script, entry, html, and rendered h1 onto the page using React

---
+++ W13D3/todos/frontend/todo_redux.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('content');
+  ReactDOM.render(<h1>Todo App</h1>, root);
+});
