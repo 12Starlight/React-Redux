f3967bd8c7099e03a18ff5b4644610819268af1d
Dave <dave.gagnat@gmail.com>
2019-12-12 19:05:10 -0600

    Added initial state for todoReducer to default too

--- W13D3/todos/frontend/reducers/todos_reducer.js
+++ W13D3/todos/frontend/reducers/todos_reducer.js
@@ -1,4 +1,20 @@
-const todosReducer = (oldState = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shaampoo',
+    done: true
+  }
+};
+
+
+const todosReducer = (oldState = initialState, action) => {
   Object.freeze(oldState);
 
   switch (action.type) {
