f29ddefbbc4811013734872fea67da5a732df198
Dave <dave.gagnat@gmail.com>
2020-02-05 16:30:26 -0600

    Created skeleton for GiphysSearch presentational component

--- W13D5/giphy_search_hw/skeleton/components/giphys_search.jsx
+++ W13D5/giphy_search_hw/skeleton/components/giphys_search.jsx
@@ -1,3 +1,32 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+import { render } from 'react-dom';
+
+
+class GiphysSearch extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      
+    }
+  }
+
+
+  render() {
+    return (
+      <div>I am a search component</div>
+    )
+  }
+}
+
+
+export default GiphysSearch;
+
+
+
+
+
+
+
